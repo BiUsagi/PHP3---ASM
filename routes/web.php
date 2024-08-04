3c57366ef9b4cbb171456909778dc0ffbb593588
BiUsagi <tubanovel@gmail.com>
2024-08-04 21:09:00 +0700

    thêm lệnh 1

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/tintuc/{id}', [HomeController::class, 'tintuc']);
-Route::get('/theloai', [HomeController::class, 'theloai']);
-Route::get('/timkiem', [HomeController::class, 'timkiem']);
-Route::get('/gioithieu', [HomeController::class, 'gioithieu']);
-Route::get('/lienhe', [HomeController::class, 'lienhe']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
